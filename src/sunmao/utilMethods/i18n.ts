86c6a917363f4fe55c9aeff24360d6aa0a07a0f6
Bowen Tan <bowen.tan@smartx.com>
2022-09-06 14:42:45 +0800

    Revert "refactor: refactor util method"
    This reverts commit 98cf610f498f3fe76d253e52fc2f02fb8d4a316e.

--- src/sunmao/utilMethods/i18n.ts
+++ src/sunmao/utilMethods/i18n.ts
@@ -3,17 +3,19 @@ import { Type } from '@sinclair/typebox';
 import i18n from '../../locales';
 
 export default function i18nUtilMethodFactory() {
-  return implementUtilMethod({
-    version: 'custom/v1',
-    metadata: {
-      name: 'i18n',
-    },
-    spec: {
-      parameters: Type.Object({
-        lang: StringUnion(['en-US', 'zh-CN']),
-      }),
-    },
-  })(({ lang }) => {
-    i18n.changeLanguage(lang);
-  });
+  return [
+    implementUtilMethod({
+      version: 'custom/v1',
+      metadata: {
+        name: 'i18n',
+      },
+      spec: {
+        parameters: Type.Object({
+          lang: StringUnion(['en-US', 'zh-CN']),
+        }),
+      },
+    })(({ lang }) => {
+      i18n.changeLanguage(lang);
+    }),
+  ];
 }
--- src/sunmao/utilMethods/navigate.ts
+++ src/sunmao/utilMethods/navigate.ts
@@ -3,17 +3,19 @@ import { Type } from '@sinclair/typebox';
 import { history } from '../../components/Router';
 
 export default function i18nUtilMethodFactory() {
-  return implementUtilMethod({
-    version: 'custom/v1',
-    metadata: {
-      name: 'navigate',
-    },
-    spec: {
-      parameters: Type.Object({
-        path: Type.String(),
-      }),
-    },
-  })(({ path }) => {
-    history.push(path);
-  });
+  return [
+    implementUtilMethod({
+      version: 'custom/v1',
+      metadata: {
+        name: 'navigate',
+      },
+      spec: {
+        parameters: Type.Object({
+          path: Type.String(),
+        }),
+      },
+    })(({ path }) => {
+      history.push(path);
+    }),
+  ];
 }
